923c1cb5040f990e55b832c17db1f3772959b00f
maj <maj@gmail.com>
2026-03-09 02:37:41 +0100

    add the re write module

--- components/nav.php
+++ components/nav.php
@@ -20,35 +20,35 @@ $currentPage = basename($_SERVER['PHP_SELF']);
                 <!--mega menu start-->
                 <ul class="menuzord-menu pull-right">
                     <li class="<?php echo ($currentPage == 'index.php') ? 'active' : ''; ?>">
-                        <a href="index.php">Home</a>
+                        <a href="/iot/">Home</a>
                     </li>
 
                     <li class="<?php echo ($currentPage == 'about.php') ? 'active' : ''; ?>">
-                        <a href="about.php">About Us</a>
+                        <a href="/iot/about">About Us</a>
                     </li>
 
                     <li class="<?php echo ($currentPage == 'whatwedo.php') ? 'active' : ''; ?>">
-                        <a href="whatwedo.php">What We Do</a>
+                        <a href="/iot/whatwedo">What We Do</a>
                     </li>
 
                     <li class="<?php echo ($currentPage == 'initiatives.php') ? 'active' : ''; ?>">
-                        <a href="initiatives.php">Initiatives</a>
+                        <a href="/iot/initiatives">Initiatives</a>
                     </li>
 
                     <li class="<?php echo ($currentPage == 'ourworks.php') ? 'active' : ''; ?>">
-                        <a href="ourworks.php">Portfolio</a>
+                        <a href="/iot/ourworks">Portfolio</a>
                     </li>
 
                     <li class="<?php echo ($currentPage == 'progressbar.php') ? 'active' : ''; ?>">
-                        <a href="progressbar.php">Focus Area</a>
+                        <a href="/iot/progressbar">Focus Area</a>
                     </li>
 
                     <li class="<?php echo ($currentPage == 'ourteam.php') ? 'active' : ''; ?>">
-                        <a href="ourteam.php">Team</a>
+                        <a href="/iot/ourteam">Team</a>
                     </li>
 
                     <li class="<?php echo ($currentPage == 'contact.php') ? 'active' : ''; ?>">
-                        <a href="contact.php">Contact</a>
+                        <a href="/iot/contact">Contact</a>
                     </li>
                 </ul>
                 <!--mega menu end-->
